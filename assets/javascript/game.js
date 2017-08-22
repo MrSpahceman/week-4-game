c13a326e3d88dc9cacb94203a432611486c54ba9
MrSpahceman <palacc25@gmail.com>
2017-08-21 21:13:19 -0700

    change

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+
+
+
+
+alert("hello");
+//Creating an object array of my crystals
+var crysTals = [
+	{	
+		name1:"RedRuby",
+		Power:10
+	},	
+	
+	{
+		name2:"YellowDiamond",
+		Power:5,
+	},
+
+	{
+		name3:"GreenRuby",
+ 		Power:7,
+	},
+	
+	{
+		name4:"BlueRuby",
+		Power:10,
+	},
+	
+];
+
+console.log(crysTals)
+
